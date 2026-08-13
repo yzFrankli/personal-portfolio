12afebfc44047080f3f5bb3b6cc2ab7864b0ca90
Frank Li <55543424+yzFrankli@users.noreply.github.com>
2026-08-13 18:40:53 -0400

    fixed bugs with robots.ts (for search engine crawlers) and added texts to the home page

--- app/og/route.tsx
+++ app/og/route.tsx
@@ -2,7 +2,7 @@ import { ImageResponse } from 'next/og'
 
 export function GET(request: Request) {
   let url = new URL(request.url)
-  let title = url.searchParams.get('title') || 'Frank (Yunzhe) Li'
+  let title = url.searchParams.get('title') || 'Frank Li'
 
   return new ImageResponse(
     (
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,10 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 className="mb-8 text-xl font-semibold tracking-tighter">
+        Frank Li
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
+      <p>some description</p>
       <div className="my-8">
         {/* <BlogPosts /> */}
       </div>
